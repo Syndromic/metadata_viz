1f40830a6de753a719300c8a5cabbb3365065dfe
Harold <stringtheory25@gmail.com>
2017-08-09 08:20:02 -0400

    Updated Metadata_Viz.R
    Used the %>% operator and the .$ operator to reduce the amount of code I need to write to accomplish the same thing. Also improved on a code comment for data import.

--- Metadata_Viz.R
+++ Metadata_Viz.R
@@ -7,40 +7,41 @@ library(magrittr)
 library(dplyr)
 library(shiny)
 
+
 ## UI ----
 ui <- fluidPage(
   leafletOutput("mymap"),
   absolutePanel(fixed = TRUE, draggable = TRUE, top = 60, bottom = "auto", left = 20, right = "auto",
                 width = 330, height = "auto",
-
-  h2("Metadata Viz App"),
-  
-  # Select completeness or timeliness domain
-  selectInput(
-    "domain", label = h3("Select data quality domain:"),
-    c(Completeness = "completeness",
-      Timeliness = "timeliness"), selected = "completeness"),
-  
-  # Only show this panel if completeness domain is selected
-  conditionalPanel(
-    condition = "input.domain == 'completeness'",
-    selectInput("field", label = h3("Select field:"), 
-              choices = list("Administrative_Sex" = "Administrative_Sex", "C_Patient_Age" = "C_Patient_Age", "Age_Calculated" = "Age_Calculated",
-                             "Age_Reported" = "Age_Reported", "Age_Units_Calculated" = "Age_Units_Calculated", "Age_Units_Reported" = "Age_Units_Reported",
-                             "Birth_Date_Time" = "Birth_Date_Time", "Patient_Zip" = "Patient_Zip", "C_Chief_Complaint" = "C_Chief_Complaint",
-                             "Admit_Reason_Description" = "Admit_Reason_Description", "Chief_Complaint_Text" = "Chief_Complaint_Text", "Admit_Reason_Combo" = "Admit_Reason_Combo",
-                             "Clinical_Impression" = "Clinical_Impression", "Triage_Notes" = "Triage_Notes", "Discharge_Disposition" = "Discharge_Disposition",
-                             "Diagnosis_Combo" = "Diagnosis_Combo"), 
-              selected = "Administrative_Sex")
-  ),
-  # Only show this panel if timeliness domain is selected
-  conditionalPanel(
-    condition = "input.domain == 'timeliness'",
-    selectInput("measure", label = h3("Select measure:"), 
-                choices = list("Percent of records received within 24 hours" = "pLessThan24", "Percent of records received within 48 hours" = "pLessThan48", "Average number of days until 80% of records received" = "d_p80"), 
-                selected = "pLessThan24", width = "450px")
+                
+                h2("Metadata Viz App"),
+                
+                # Select completeness or timeliness domain
+                selectInput(
+                  "domain", label = h3("Select data quality domain:"),
+                  c(Completeness = "completeness",
+                    Timeliness = "timeliness"), selected = "completeness"),
+                
+                # Only show this panel if completeness domain is selected
+                conditionalPanel(
+                  condition = "input.domain == 'completeness'",
+                  selectInput("field", label = h3("Select field:"), 
+                              choices = list("Administrative_Sex" = "Administrative_Sex", "C_Patient_Age" = "C_Patient_Age", "Age_Calculated" = "Age_Calculated",
+                                             "Age_Reported" = "Age_Reported", "Age_Units_Calculated" = "Age_Units_Calculated", "Age_Units_Reported" = "Age_Units_Reported",
+                                             "Birth_Date_Time" = "Birth_Date_Time", "Patient_Zip" = "Patient_Zip", "C_Chief_Complaint" = "C_Chief_Complaint",
+                                             "Admit_Reason_Description" = "Admit_Reason_Description", "Chief_Complaint_Text" = "Chief_Complaint_Text", "Admit_Reason_Combo" = "Admit_Reason_Combo",
+                                             "Clinical_Impression" = "Clinical_Impression", "Triage_Notes" = "Triage_Notes", "Discharge_Disposition" = "Discharge_Disposition",
+                                             "Diagnosis_Combo" = "Diagnosis_Combo"), 
+                              selected = "Administrative_Sex")
+                ),
+                # Only show this panel if timeliness domain is selected
+                conditionalPanel(
+                  condition = "input.domain == 'timeliness'",
+                  selectInput("measure", label = h3("Select measure:"), 
+                              choices = list("Percent of records received within 24 hours" = "pLessThan24", "Percent of records received within 48 hours" = "pLessThan48", "Average number of days until 80% of records received" = "d_p80"), 
+                              selected = "pLessThan24", width = "450px")
+                )
   )
- )
 )
 
 ## Server ----
@@ -48,17 +49,17 @@ server <- function(input, output) {
   
   ## Import Metadata Workgroup data ----
   Timeliness <- import("NSSP_MDWG_aYear.xlsx", which=5)
-  Completeness <- suppressWarnings(import("NSSP_MDWG_aYear.xlsx", which=6)) # Ignore warnings. These are triggered by odd entries (NR instead of %) but they are processed correctly (as NAs).
+  Completeness <- suppressWarnings(import("NSSP_MDWG_aYear.xlsx", which=6, col_types=c("timevar"="numeric", "site_id"="numeric", "Site_Name"="text", "Site_State"="text", "dqgrp"="numeric", "dqgrp2"="numeric", "dqvar"="text", "pvisits"="numeric"))) # Ignore warnings. These are triggered by "NR" character entries instead of numeric values in the pvisits field. They end up being processed correctly (as NAs) so it is OK.
   
   ## Clean and format Timeliness data ----
-  Timeliness_Extended <- mutate(Timeliness, 
+  Timeliness_Extended <- Timeliness %>% mutate(
                                 Geographic_Level = case_when(
-                                  str_detect(Timeliness$Site_Name, ",") == 1 ~ "County",
-                                  str_detect(Timeliness$Site_Name, ",") == 0 ~ "State"
+                                  str_detect(.$Site_Name, ",") == 1 ~ "County",
+                                  str_detect(.$Site_Name, ",") == 0 ~ "State"
                                 ),
                                 County_Name = case_when(
-                                  str_detect(Timeliness$Site_Name, ",") == 1 ~ str_split_fixed(Timeliness$Site_Name, ",", 2)[ , 1],
-                                  str_detect(Timeliness$Site_Name, ",") == 0 ~ NA_character_ 
+                                  str_detect(.$Site_Name, ",") == 1 ~ str_split_fixed(.$Site_Name, ",", 2)[ , 1],
+                                  str_detect(.$Site_Name, ",") == 0 ~ NA_character_ 
                                 ),
                                 pLessThan24 = (pLessThan24 * 100) %>% round(2),
                                 pLessThan48 = (pLessThan48 * 100) %>% round(2),
@@ -66,45 +67,42 @@ server <- function(input, output) {
   )
   
   ## Clean and format Completeness data ----
-  Completeness_Extended <- mutate(Completeness, 
+  Completeness_Extended <- Completeness %>% mutate( 
                                   Geographic_Level = case_when(
-                                    str_detect(Completeness$Site_Name, ",") == 1 ~ "County",
-                                    str_detect(Completeness$Site_Name, ",") == 0 ~ "State"
+                                    str_detect(.$Site_Name, ",") == 1 ~ "County",
+                                    str_detect(.$Site_Name, ",") == 0 ~ "State"
                                   ),
                                   County_Name = case_when(
-                                    str_detect(Completeness$Site_Name, ",") == 1 ~ str_split_fixed(Completeness$Site_Name, ",", 2)[ , 1],
-                                    str_detect(Completeness$Site_Name, ",") == 0 ~ NA_character_ 
+                                    str_detect(.$Site_Name, ",") == 1 ~ str_split_fixed(.$Site_Name, ",", 2)[ , 1],
+                                    str_detect(.$Site_Name, ",") == 0 ~ NA_character_ 
                                   ),
-                                  pvisits = case_when(
-                                    is.na(Completeness$pvisits) == TRUE ~ NA_real_, 
-                                    is.na(Completeness$pvisits) == FALSE ~ (Completeness$pvisits * 100) %>% round(2)
-                                  )
+                                  pvisits = (pvisits * 100) %>% round(2)
   )
-  
+
   ## Load spatial data and merge with dataframes ----
   
-    # Load U.S. State and County spatial data frames
-    us_states <- states(cb = TRUE, resolution = '20m', year = 2015)
-    us_counties <- counties(cb = TRUE, resolution = '20m', year = 2015)
+  # Load U.S. State and County spatial data frames
+  us_states <- states(cb = TRUE, resolution = '20m', year = 2015)
+  us_counties <- counties(cb = TRUE, resolution = '20m', year = 2015)
   
-    # Map code only
-    output$mymap <- renderLeaflet({
+  # Map code only
+  output$mymap <- renderLeaflet({
     
     # If completeness domain selected
     if(input$domain == "completeness"){  
-    # Filter by field and county  
+      # Filter by field and county  
       state_data <- filter(Completeness_Extended, dqvar == input$field, Geographic_Level == "State") %>%
-                    rename(measure = pvisits)
+        rename(measure = pvisits)
       county_data <- filter(Completeness_Extended, dqvar == input$field, Geographic_Level == "County") %>%
-                    rename(measure = pvisits)
+        rename(measure = pvisits)
     }
     
     # If timeliness domain is selected  
     if (input$domain == "timeliness"){
       state_data <- filter(Timeliness_Extended, Geographic_Level == "State") %>%
-                    rename_(measure = input$measure)
+        rename_(measure = input$measure)
       county_data <- filter(Timeliness_Extended, Geographic_Level == "County") %>%
-                    rename_(measure = input$measure)      
+        rename_(measure = input$measure)      
     } 
     
     # Associate county data with geocodes
